c6b4c2827b05e3f46d4c72ae91df878a93226f21
roaco <roaco_91@hotmail.com>
2022-06-14 01:37:06 -0500

    Se modifican consultas

--- src/main/java/co/edu/uniquindio/PF_ANALISIS/Repositories/EstudianteRepo.java
+++ src/main/java/co/edu/uniquindio/PF_ANALISIS/Repositories/EstudianteRepo.java
@@ -12,16 +12,10 @@ public interface EstudianteRepo extends JpaRepository<Estudiante, Integer> {
     /**
      * Obtiene todos los estudiantes ordenandolos segun la calificacion del test.
      */
-    @Query(value = "SELECT nombre, id_estudiante, sum(valoracion) nota FROM respuesta " +
-            "join estudiante on estudiante.codigo = respuesta.id_estudiante " +
-            "group by id_estudiante " +
-            "order by 2 desc", nativeQuery = true)
+    @Query(value = "SELECT nombre, id_estudiante, sum(valoracion) nota FROM respuesta join estudiante on estudiante.codigo = respuesta.id_estudiante group by id_estudiante order by 2 desc", nativeQuery = true)
     List<Object[]> getEstudiantesPuntaje();
 
-    @Query(value = "SELECT nombre, codigo, enunciado, valoracion FROM respuesta R\n" +
-            "join estudiante E on E.codigo = R.id_estudiante\n" +
-            "join pregunta P on P.id_pregunta = R.id_pregunta\n" +
-            "where codigo = ?", nativeQuery = true)
+    @Query(value = "SELECT nombre, codigo, enunciado, valoracion FROM respuesta R join estudiante E on E.codigo = R.id_estudiante join pregunta P on P.id_pregunta = R.id_pregunta where codigo = ?", nativeQuery = true)
     List<Object[]> getResultadosTest(Integer codigo);
 
 }
--- src/main/java/co/edu/uniquindio/PF_ANALISIS/Repositories/RespuestaRepo.java
+++ src/main/java/co/edu/uniquindio/PF_ANALISIS/Repositories/RespuestaRepo.java
@@ -10,22 +10,9 @@ import java.util.List;
 @Repository
 public interface RespuestaRepo extends JpaRepository<Respuesta, Integer> {
 
-    @Query(value = "select p.enunciado, " +
-            "(select count(*)  " +
-            "from respuesta rr " +
-            "where rr.valoracion = 1 " +
-            "and rr.id_pregunta = r.id_pregunta ) correctas, " +
-            "(select count(*) " +
-            "from respuesta rr " +
-            "where rr.valoracion = 0 " +
-            "and rr.id_pregunta = r.id_pregunta ) incorrectas from respuesta r join pregunta p " +
-            "on r.id_pregunta = p.id_pregunta", nativeQuery = true)
+    @Query(value = "select p.enunciado, (select count(*) from respuesta rr where rr.valoracion = 1 and rr.id_pregunta = r.id_pregunta ) correctas, (select count(*) from respuesta rr where rr.valoracion = 0 and rr.id_pregunta = r.id_pregunta ) incorrectas from respuesta r join pregunta p on r.id_pregunta = p.id_pregunta", nativeQuery = true)
     List<Object[]> getPreguntasTYF();
 
-    @Query(value = "select p.enunciado, avg(valoracion) Promedio_Global " +
-            "from respuesta r join pregunta p " +
-            "on p.id_pregunta = r.id_pregunta " +
-            "group by p.enunciado " +
-            "order by 1", nativeQuery = true)
+    @Query(value = "select p.enunciado, avg(valoracion) Promedio_Global from respuesta r join pregunta p on p.id_pregunta = r.id_pregunta group by p.enunciado order by 1", nativeQuery = true)
     List<Object[]> getPromedioPreguntas();
 }
